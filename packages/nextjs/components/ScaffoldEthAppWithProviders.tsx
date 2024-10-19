1413a585a4ca520ca9a7f31c113f26d0475832bf
fffilps <jmariwyatt@gmail.com>
2024-10-19 15:16:14 -0700

    modified the footer to look better

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -22,7 +22,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
     <>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="relative flex flex-col flex-1">{children}</main>
+        <main className="relative flex flex-col flex-1 bg-blue-100">{children}</main>
         <Footer />
       </div>
       <Toaster />
